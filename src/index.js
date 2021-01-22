6eabc69eeb88ae198a2edb5fb6985d3b71259f23
Bryan <bryan.roberts.home@gmail.com>
2021-01-21 20:18:02 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hi</h1>
+    <ul>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
